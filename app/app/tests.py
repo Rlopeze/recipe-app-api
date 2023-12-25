1762d268c267c8824db5bbf3c744bcb60d9e4693
Rlopeze <mumolopeze@uc.cl>
2023-12-25 11:14:06 -0300

    database

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ Sample tests
 from django.test import TestCase
 from app import calc
 
+
 class CalcTests(TestCase):
     """
     Test the calc module.
@@ -14,7 +15,7 @@ class CalcTests(TestCase):
         Test that two numbers are added together
         """
         self.assertEqual(calc.add(3, 8), 11)
-    
+
     def test_subtract_numbers(self):
         """
         Test that values are subtracted and returned
